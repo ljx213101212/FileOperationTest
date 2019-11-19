856ef409654bfe0040a17c998f29317a0a340d3b
Li Jixiang <jixiang.li@razer.com>
2019-11-19 13:37:58 +0800

    First commit.

---
+++ FileStream/FileStream/FileStream.cpp
@@ -0,0 +1,160 @@
+// FileStream.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <stdio.h>
+#include <cstdio>
+#include <sys/stat.h>
+#include <string>
+#include <vector>
+
+
+#include <wincrypt.h>
+#include <WinTrust.h>
+#include <Softpub.h>
+
+#pragma comment (lib, "wintrust")
+#pragma comment (lib, "crypt32.lib")
+#define ENCODING (X509_ASN_ENCODING | PKCS_7_ASN_ENCODING)
+
+#define ReadBufferSize 31
+using namespace std;
+
+std::string GetLastErrorAsString()
+{
+	//Get the error message, if any.
+	DWORD errorMessageID = ::GetLastError();
+	if (errorMessageID == 0)
+		return std::string(); //No error message has been recorded
+
+	LPSTR messageBuffer = nullptr;
+	size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+
+	std::string message(messageBuffer, size);
+
+	//Free the buffer.
+	LocalFree(messageBuffer);
+
+	return message;
+}
+
+static bool CheckSignature(const std::wstring& dllPath, HANDLE& hfile, std::wstring& reason) {
+	bool isTrusted = false;
+	std::vector<std::string> certificateChain;
+
+	WINTRUST_FILE_INFO file_info = { 0 };
+	file_info.cbStruct = sizeof(file_info);
+	//file_info.pcwszFilePath = dllPath.c_str();
+	file_info.hFile = hfile;
+	file_info.pgKnownSubject = NULL;
+
+	WINTRUST_DATA wintrust_data = { 0 };
+	wintrust_data.cbStruct = sizeof(wintrust_data);
+	wintrust_data.pPolicyCallbackData = NULL;
+	wintrust_data.pSIPClientData = NULL;
+	wintrust_data.dwUIChoice = WTD_UI_NONE;
+	wintrust_data.fdwRevocationChecks = WTD_REVOKE_NONE;
+	wintrust_data.dwUnionChoice = WTD_CHOICE_FILE;
+	wintrust_data.pFile = &file_info;
+	wintrust_data.dwStateAction = WTD_STATEACTION_VERIFY;
+	wintrust_data.hWVTStateData = NULL;
+	wintrust_data.pwszURLReference = NULL;
+	// Disallow revocation checks over the network.
+	wintrust_data.dwProvFlags = WTD_CACHE_ONLY_URL_RETRIEVAL;
+	wintrust_data.dwUIContext = WTD_UICONTEXT_EXECUTE;
+
+	// The WINTRUST_ACTION_GENERIC_VERIFY_V2 policy verifies that the certificate
+	// chains up to a trusted root CA, and that it has appropriate permission to
+	// sign code.
+	GUID policy_guid = WINTRUST_ACTION_GENERIC_VERIFY_V2;
+
+	LONG result = WinVerifyTrust(static_cast<HWND>(INVALID_HANDLE_VALUE), &policy_guid, &wintrust_data);
+
+	CRYPT_PROVIDER_DATA* prov_data = WTHelperProvDataFromStateData(wintrust_data.hWVTStateData);
+	if (prov_data) {
+		if (prov_data->csSigners > 0) {
+			if (result == ERROR_SUCCESS) {
+				isTrusted = true;
+			}
+		}
+		// Free the provider data.
+		wintrust_data.dwStateAction = WTD_STATEACTION_CLOSE;
+		WinVerifyTrust(static_cast<HWND>(INVALID_HANDLE_VALUE), &policy_guid, &wintrust_data);
+	}
+
+	if (!isTrusted) {
+		reason = dllPath + L" not trusted";
+		return false;
+	}
+	return true;
+}
+
+int main()
+{
+	//ofstream myfile;
+	/*string filePath = "C:\\Users\\jixiang.li\\AppData\\Local\\Chromium\\User Data\\Apps\\helloworld_folder\\dll\\ThxNativeLite.dll";*/
+	string filePath = "D:\\downloadTest\\sample.exe";
+	
+	std::ifstream input(filePath, std::ios::binary);
+	// copies all data into buffer
+	std::vector<char> buffer(std::istreambuf_iterator<char>(input), {});
+
+	//FILE* memFile = open_memstream(buffer, buffer.size());
+	
+
+	HANDLE WINHandle = CreateFile(TEXT("D:\\downloadTest\\hello"), (GENERIC_WRITE | GENERIC_READ), (FILE_SHARE_READ | FILE_SHARE_WRITE |
+		FILE_SHARE_DELETE), 0, CREATE_ALWAYS, (FILE_ATTRIBUTE_TEMPORARY | FILE_FLAG_DELETE_ON_CLOSE), 0); // (FILE_ATTRIBUTE_TEMPORARY) | FILE_FLAG_DELETE_ON_CLOSE
+
+	DWORD bytesWritten;
+	char *str = reinterpret_cast<char*>(buffer.data());
+	WriteFile(WINHandle, str, buffer.size(), &bytesWritten, NULL);
+
+	//DWORD  dwCurrentFilePosition = SetFilePointer(
+	//	WINHandle, // must have GENERIC_READ and/or GENERIC_WRITE
+	//	0, // do not move pointer
+	//	NULL, // hFile is not large enough in needing this 
+	//	FILE_BEGIN); // provides offset from current position
+
+	//printf("Current file pointer position: %d\n", dwCurrentFilePosition);
+
+	//char buff[ReadBufferSize];
+	//DWORD dwBytesRead = 0;
+	//bool isSuccess = ReadFile(WINHandle, buff, ReadBufferSize - 1, &dwBytesRead, NULL);
+	//if (isSuccess) {
+	//	buff[dwBytesRead] = '\0';
+	//	printf("%s\n", buff);
+	//}
+
+	//string s = GetLastErrorAsString();
+	//cout << s << endl;
+
+	//HANDLE WINHandle2 = CreateFile(TEXT("D:\\downloadTest\\hello"), (GENERIC_WRITE | GENERIC_READ), (FILE_SHARE_READ | FILE_SHARE_WRITE |
+	//	FILE_SHARE_DELETE), 0, OPEN_EXISTING, (FILE_ATTRIBUTE_TEMPORARY | FILE_FLAG_DELETE_ON_CLOSE), 0); // (FILE_ATTRIBUTE_TEMPORARY) | FILE_FLAG_DELETE_ON_CLOSE
+
+
+	//char buff[ReadBufferSize];
+	//DWORD dwBytesRead = 0;
+	//bool isSuccess = ReadFile(WINHandle2, buff, ReadBufferSize - 1, &dwBytesRead, NULL);
+	//if (isSuccess) {
+	//	buff[dwBytesRead] = '\0';
+	//	printf("%s\n", buff);
+	//}
+	//string s = GetLastErrorAsString();
+	//cout << s << endl;
+
+	std::wstring reason;
+
+	HANDLE try1 = NULL;
+	//int* ptr = 0;
+	//*ptr = 1;
+	bool isRazerSigned = CheckSignature(TEXT(""), WINHandle, reason);
+
+
+
+	CloseHandle(WINHandle);
+	//CloseHandle(WINHandle2);
+	return 1;
+}
---
+++ FileWrite/FileWrite/FileWrite.cpp
@@ -0,0 +1,157 @@
+// FileWrite.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <Windows.h>
+#include <fstream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+//http://www.libpng.org/pub/png/book/chapter11.html#png.ch11.div.14
+//International Text Annotations (iTXt)
+//https://github.com/finnp/png-itxt/blob/master/index.js  (REf from javascript implementation)
+
+ //79 + 5 + 0 + 0 + 0 + 10000
+#define bufferSize 200
+#define imageSize  0xF4240
+#define BYTES_PER_DWORD 5
+
+typedef struct ITXTDATA
+{
+	char* keyword;
+	char* value;
+} iTxt;
+
+std::ifstream::pos_type filesize(const char* filename)
+{
+	std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
+	return in.tellg();
+}
+
+void DwordsToBytesArray(const DWORD* dwIn, BYTE* bOut, const UINT dwCount,
+	const UINT bCount) {
+	UINT i = 0, j = 0, k = 0, l = 0;
+	BYTE bBuff[BYTES_PER_DWORD];
+
+	for (i = 0; i < dwCount; i++) {
+		*(DWORD*)bBuff = dwIn[i];
+		for (k = BYTES_PER_DWORD; k > 0; k--) {
+			if (j < bCount) {
+				bOut[j++] = bBuff[k - 1];
+			}
+			else {
+				break;
+			}
+		}
+	}
+}
+
+//working version
+void writeITXT(HANDLE fHandle, size_t fileSize, iTxt data) {
+	
+	int keyLen = min(79, strlen(data.keyword));
+	DWORD bytesWritten = 0;
+	DWORD bytesWrittenCurr;
+	int totalLength = 0;
+	DWORD hexValue = strlen(data.keyword) + strlen(data.value);
+	char bOut[BYTES_PER_DWORD] = {0x00,0x00,0x00,(char)hexValue};
+	char chunkType[BYTES_PER_DWORD] = {0x69,0x54,0x58,0x74};
+	char hardCodeCRC[BYTES_PER_DWORD] = { 0x40, 0x75,0x49,0xF3 };
+	
+	
+	string buffer = string(chunkType) + string(data.keyword) + string(data.value) + string(hardCodeCRC);
+	totalLength =  sizeof(chunkType) - 1 + strlen(data.keyword) + strlen(data.value) + sizeof(hardCodeCRC) - 1;
+	const char* str = buffer.c_str();
+	const char* target = "IHDR";
+	char* srcBuff = new char[imageSize];
+	DWORD dwBytesRead = 0;
+	bool isSuccess = ReadFile(fHandle, srcBuff, imageSize - 1, &dwBytesRead, NULL);
+
+	int targetIdx = -1;
+	for (DWORD i = 0; i < imageSize; i++) {
+		if (srcBuff[i] == 0x49) {
+			char checkStr[5] = { '\0' };
+			memcpy_s(checkStr, sizeof target, srcBuff + i, sizeof target);
+			string checkS(checkStr);
+			string targetS(target);
+			if (checkS == targetS) {
+				targetIdx = i;
+				break;
+			}
+		}
+	}
+	ptrdiff_t offset = (targetIdx) - 4;
+
+	if (SetFilePointer(fHandle, offset, NULL, FILE_BEGIN) == INVALID_SET_FILE_POINTER)
+	{
+		cout << "SetFilePointer error!" << endl;
+	}
+
+	DWORD dwSignatureSizeBigEndian = _byteswap_ulong(hexValue);
+	WriteFile(fHandle, &dwSignatureSizeBigEndian, sizeof(DWORD), &bytesWrittenCurr, NULL);
+	bytesWritten += bytesWrittenCurr;
+	char* lastPartBuffs = new char[imageSize];
+	memcpy_s(lastPartBuffs, imageSize - offset, srcBuff + offset, imageSize - offset);
+	WriteFile(fHandle, str, totalLength, &bytesWrittenCurr, NULL);
+	bytesWritten += bytesWrittenCurr;
+	WriteFile(fHandle, lastPartBuffs, fileSize - offset, &bytesWrittenCurr, NULL);
+	bytesWritten += bytesWrittenCurr;
+
+	delete lastPartBuffs;
+	delete srcBuff;
+}
+
+int main()
+{
+	wstring inputPath = L"D:\\SourceCodeTest\\media\\png\\png-itxt\\image\\sample2.png";
+	HANDLE hFile;
+	//string filePath = "D:\\downloadTest\\sample.exe";
+	//std::ifstream input(filePath, std::ios::binary);
+	//// copies all data into buffer
+	//std::vector<char> buffer(std::istreambuf_iterator<char>(input), {});
+
+	hFile = CreateFile((LPCWSTR)inputPath.c_str(),                // name of the write
+		GENERIC_READ | GENERIC_WRITE,          // open for writing
+		(FILE_SHARE_READ | FILE_SHARE_WRITE |
+			FILE_SHARE_DELETE),                      // do not share
+		NULL,                   // default security
+		OPEN_EXISTING,             // open existing file only
+		FILE_ATTRIBUTE_NORMAL,  // normal file
+		NULL);                  // no attr. template
+
+
+	char keyword[100] = "keyword";
+	char value[10000] = "value";
+	iTxt data = {
+		keyword,//123123
+		value//123123
+	};
+
+	size_t fileSize = filesize("D:\\SourceCodeTest\\media\\png\\png-itxt\\image\\sample2.png");
+	writeITXT(hFile, fileSize, data);
+	CloseHandle(hFile);
+
+
+	const char* s = "123";
+	const char* s1 = "456";
+
+	char buffer[1000] = "";
+	strcat_s(buffer, sizeof buffer,  s);
+	strcat_s(buffer, sizeof buffer, s1);
+	std::cout << sizeof buffer  << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
